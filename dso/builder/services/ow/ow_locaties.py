7fd5f450f5b169010a93ac8ecf767d43f23bbe7a
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2024-01-15 22:45:07 +0100

    gebied is not required

--- dso/builder/services/ow/ow_locaties.py
+++ dso/builder/services/ow/ow_locaties.py
@@ -70,7 +70,7 @@ class OwLocatiesContent:
             {gebiedengroep.geo_uuid: gebiedengroep.OW_ID for gebiedengroep in self.xml_data["gebiedengroepen"]}
         )
         for object_code, values in self.object_tekst_lookup.items():
-            if not "gebied_uuid" in values:
+            if values.get("gebied_uuid", None) is None:
                 continue
             # Find the matching OWGebied and update ow_location_id to the state
             matching_ow_gebied = ow_gebied_mapping.get(UUID(values["gebied_uuid"]))
