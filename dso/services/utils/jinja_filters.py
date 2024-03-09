62035746e48b5a60c72472385559e629ff77a689
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2024-03-09 12:02:08 +0100

    Add string check for has_text filter

--- dso/services/utils/jinja_filters.py
+++ dso/services/utils/jinja_filters.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 
 
 def jinja2_filter_has_text(value):
+    if not isinstance(value, str):
+        return False
+
     soup = BeautifulSoup(value, "html.parser")
     text = soup.get_text().strip()
 
