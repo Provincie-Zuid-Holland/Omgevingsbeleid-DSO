21131c8e0c68b6acecd67fbdd955b59119f4ddbc
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2024-03-12 17:33:33 +0100

    parse some html to xml

--- dso/builder/services/aanlevering_besluit/besluit_versie/besluit_compact_content.py
+++ dso/builder/services/aanlevering_besluit/besluit_versie/besluit_compact_content.py
@@ -1,4 +1,8 @@
+from bs4 import BeautifulSoup
+
+from .....services.tekst.tekst import Inhoud
 from .....services.utils.helpers import load_template
+from ....state_manager.input_data.besluit import Besluit
 from ....state_manager.state_manager import StateManager
 from .besluit_compact.artikelen_content import ArtikelenContent
 from .besluit_compact.wijzig_bijlage_content import WijzigBijlageContent
@@ -12,10 +16,28 @@ class BesluitCompactContent:
         artikelen_lichaam: str = ArtikelenContent(self._state_manager).create()
         wijzig_bijlage: str = WijzigBijlageContent(self._state_manager).create()
 
+        besluit: Besluit = self._state_manager.input_data.besluit
+        aanhef_xml: str = self._html_to_xml_without_root(besluit.aanhef)
+        sluiting_xml: str = self._html_to_xml_without_root(besluit.sluiting)
+        ondertekening_xml: str = self._html_to_xml_without_root(besluit.ondertekening)
+
         content = load_template(
             "akn/besluit_versie/BesluitCompact.xml",
-            besluit=self._state_manager.input_data.besluit,
+            besluit=besluit,
             artikelen_lichaam=artikelen_lichaam,
             wijzig_bijlage=wijzig_bijlage,
+            aanhef_xml=aanhef_xml,
+            sluiting_xml=sluiting_xml,
+            ondertekening_xml=ondertekening_xml,
         )
         return content
+
+    def _html_to_xml_without_root(self, html: str) -> str:
+        input_soup = BeautifulSoup(html, "html.parser")
+        lichaam = Inhoud()
+        lichaam.consume_children(input_soup.children)
+
+        output_soup = BeautifulSoup(features="xml")
+        output = lichaam.as_xml(output_soup)
+        contents_without_root = "".join(str(child) for child in output.contents)
+        return contents_without_root
