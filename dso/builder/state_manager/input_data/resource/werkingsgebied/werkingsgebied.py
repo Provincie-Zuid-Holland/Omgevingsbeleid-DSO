aa0ac3adc83f72fb06a69eb6b722bef9cdea24b2
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2024-03-15 21:47:28 +0100

    provide frbr for werkingsgebied

--- dso/builder/state_manager/input_data/resource/werkingsgebied/werkingsgebied.py
+++ dso/builder/state_manager/input_data/resource/werkingsgebied/werkingsgebied.py
@@ -1,6 +1,5 @@
 import re
 import uuid
-from datetime import datetime
 from typing import List, Optional
 
 from pydantic import BaseModel, Field, root_validator
@@ -12,8 +11,6 @@ class Locatie(BaseModel):
     UUID: uuid.UUID
     Title: str
     Symbol: str
-    Created_Date: datetime
-    Modified_Date: datetime
 
     Gml: Optional[str] = Field(None)
     Geometry: Optional[str] = Field(None)
@@ -27,49 +24,29 @@ class Locatie(BaseModel):
 
 class Werkingsgebied(BaseModel):
     UUID: uuid.UUID
-    Object_ID: Optional[int] = Field(None)
-    Object_Type: Optional[str] = Field(None)
     Code: str
     New: bool
+    Work: str
+    Expression_Version: str
     Title: str
     Symbol: str
-    Created_Date: datetime
-    Modified_Date: datetime
     Achtergrond_Verwijzing: str
     Achtergrond_Actualiteit: str
     Locaties: List[Locatie] = Field(..., alias="Onderverdelingen")
 
-    # Provided by the system instead of the resource json
-    provincie_id: str
-    expression_taal: str
-
     def get_FRBR(self) -> FRBR:
-        identifier: str = self.get_identifier()
-        work_datum: str = f"{self.Created_Date.year}"
-        work: str = f"/join/id/regdata/{self.provincie_id}/{work_datum}/{identifier}"
-
-        version: str = self.get_version(self.expression_taal)
-        expression: str = f"{work}/{version}"
+        expression: str = f"{self.Work}/{self.Expression_Version}"
 
         return FRBR(
-            work=work,
+            work=self.Work,
             expression=expression,
         )
-
-    def get_identifier(self) -> str:
-        identifier = str(self.Object_ID)
-        return identifier
 
     def get_name(self) -> str:
         s: str = self.Title.lower()
         s = re.sub(r"[^a-z0-9 ]+", "", s)
         s = s.replace(" ", "-")
         return s
-
-    def get_version(self, expression_taal: str) -> str:
-        date_version: str = self.Modified_Date.strftime("%Y-%m-%d;%H%M")
-        version: str = f"{expression_taal}@{date_version}"
-        return version
 
     def get_gml_filename(self) -> str:
         return f"locaties_{self.get_name()}.gml"
