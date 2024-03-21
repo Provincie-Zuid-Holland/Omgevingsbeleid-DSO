6ad36fa356d2294d1bd29e980df8727afa4a8e3b
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2024-03-21 22:25:15 +0100

    Fix frbr work

--- dso/builder/services/aanlevering_besluit/besluit_versie/expression_identificatie_content.py
+++ dso/builder/services/aanlevering_besluit/besluit_versie/expression_identificatie_content.py
@@ -1,6 +1,7 @@
 from .....services.utils.helpers import load_template
 from .....services.utils.waardelijsten import WorkType
 from ....state_manager.state_manager import StateManager
+from .....models import BillFRBR
 
 
 class ExpressionIdentificatieContent:
@@ -8,11 +9,11 @@ class ExpressionIdentificatieContent:
         self._state_manager: StateManager = state_manager
 
     def create(self) -> str:
-        besluit_frbr = self._state_manager.input_data.publication_settings.besluit_frbr
+        besluit_frbr: BillFRBR = self._state_manager.input_data.publication_settings.besluit_frbr
         content = load_template(
             "akn/besluit_versie/ExpressionIdentificatie.xml",
-            work=besluit_frbr.work,
-            expression=besluit_frbr.expression,
+            work=besluit_frbr.get_work(),
+            expression=besluit_frbr.get_expression(),
             soort_work=WorkType.Besluit.value,
         )
         return content
