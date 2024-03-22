69b9f7d8e0d86db174df6f1661ccd043abb15878
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2024-03-22 10:41:20 +0100

    fix wid reading

--- dso/builder/services/aanlevering_besluit/besluit_versie/besluit_compact/wijzig_bijlage/lichaam/regeling_vrijetekst_tekst_generator.py
+++ dso/builder/services/aanlevering_besluit/besluit_versie/besluit_compact/wijzig_bijlage/lichaam/regeling_vrijetekst_tekst_generator.py
@@ -101,11 +101,9 @@ class RegelingVrijetekstTekstGenerator:
         return output
 
     def _store_used_wids(self, xml_data: str):
-        soup = BeautifulSoup(xml_data, "lxml")
-        elements_with_wid = soup.find_all(attrs={"wId": True})
+        soup = BeautifulSoup(xml_data, "lxml-xml")
+        elements_with_wid = soup.find_all(attrs={"wId": True}, recursive=True)
+
         used_wids: List[str] = [element["wId"] for element in elements_with_wid]
-
-
-        self._state_manager.debug["0-found-wids"] = copy(used_wids)
         unique_used_wids: List[str] = list(set(self._state_manager.used_wids + used_wids))
         self._state_manager.used_wids = unique_used_wids
