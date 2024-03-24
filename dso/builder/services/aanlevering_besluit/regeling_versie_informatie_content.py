f0870fc50bed75dcb5300204495c6c0cfc23d40f
Jordy Moos <4688326+JordyMoos@users.noreply.github.com>
2024-03-24 12:55:43 +0100

    do not send regeling metadata on a mutation

--- dso/builder/services/aanlevering_besluit/regeling_versie_informatie_content.py
+++ dso/builder/services/aanlevering_besluit/regeling_versie_informatie_content.py
@@ -9,10 +9,29 @@ class RegelingVersieInformatieContent:
         self._state_manager: StateManager = state_manager
 
     def create(self) -> str:
-        soort_regeling = self._get_soort_regeling()
+        regeling_metadata: str = self._get_regeling_metadata()
 
         content = load_template(
             "akn/RegelingVersieInformatie.xml",
+            regeling_metadata=regeling_metadata,
+            regeling_frbr=self._state_manager.input_data.publication_settings.regeling_frbr,
+            regeling=self._state_manager.input_data.regeling,
+        )
+        return content
+
+    def _get_soort_regeling(self) -> str:
+        document_type: DocumentType = self._state_manager.input_data.publication_settings.document_type.value
+        return RegelingType[document_type].value
+
+    def _get_regeling_metadata(self) -> str:
+        # Regeling metadata is only needed for initial version
+        if self._state_manager.input_data.regeling_mutatie is not None:
+            return ""
+
+        soort_regeling = self._get_soort_regeling()
+
+        content = load_template(
+            "akn/RegelingMetadata.xml",
             soort_regeling=soort_regeling,
             regeling_frbr=self._state_manager.input_data.publication_settings.regeling_frbr,
             regeling=self._state_manager.input_data.regeling,
@@ -20,7 +39,3 @@ class RegelingVersieInformatieContent:
             soort_bestuursorgaan=self._state_manager.input_data.publication_settings.soort_bestuursorgaan,
         )
         return content
-
-    def _get_soort_regeling(self) -> str:
-        document_type: DocumentType = self._state_manager.input_data.publication_settings.document_type.value
-        return RegelingType[document_type].value
