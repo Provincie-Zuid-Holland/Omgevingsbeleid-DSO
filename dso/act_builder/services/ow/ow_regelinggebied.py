3a3ab172116e7e4ba922ec16ea920f47f4f6172f
awiseguy <arvinwijsman@gmail.com>
2024-04-29 21:22:26 +0200

    refactor ow object building and add mutations to state

--- dso/act_builder/services/ow/ow_regelinggebied.py
+++ dso/act_builder/services/ow/ow_regelinggebied.py
@@ -6,9 +6,10 @@ from ....services.ow.models import OWRegelingsgebied
 from ....services.ow.ow_id import generate_ow_id
 from ....services.utils.helpers import load_template
 from ...state_manager.models import OutputFile, StrContentData
+from ...state_manager.states.ow_repository import OWStateRepository
 
 
-class OwRegelingsgebiedContent:
+class OwRegelingsgebiedBuilder:
     """
     Prepares the content for the OWRegelingGebied
     Assuming that the regelinggebied is always part of a publication
@@ -21,16 +22,18 @@ class OwRegelingsgebiedContent:
         self,
         provincie_id: str,
         levering_id,
-        ow_procedure_status: Optional[OwProcedureStatus],
+        ow_repository: OWStateRepository,
         ambtsgebied_ow_id: str,
+        ow_procedure_status: Optional[OwProcedureStatus],
     ):
-        self.provincie_id: str = provincie_id
-        self.levering_id = levering_id
+        self._provincie_id: str = provincie_id
+        self._levering_id = levering_id
         self.ow_procedure_status = ow_procedure_status
-        self.ambtsgebied = ambtsgebied_ow_id
-        self.xml_data = {
+        self._ow_repository = ow_repository
+        self._ambtsgebied = ambtsgebied_ow_id
+        self._xml_data = {
             "filename": "owRegelingsgebied.xml",
-            "leveringsId": self.levering_id,
+            "leveringsId": self._levering_id,
             "objectTypen": [],
             "regelingsgebieden": [],
         }
@@ -38,10 +41,10 @@ class OwRegelingsgebiedContent:
 
     def create_regelingsgebieden(self):
         regelingsgebied = self._create_regelingsgebied()
-        self.xml_data["regelingsgebieden"].append(regelingsgebied)
-        self.xml_data["objectTypen"].append(OwRegelingsgebiedObjectType.REGELINGSGEBIED.value)
+        self._xml_data["regelingsgebieden"].append(regelingsgebied)
+        self._xml_data["objectTypen"].append(OwRegelingsgebiedObjectType.REGELINGSGEBIED.value)
         self.file = self.create_file()
-        return self.xml_data
+        return self._xml_data
 
     def _create_regelingsgebied(self):
         """
@@ -49,10 +52,10 @@ class OwRegelingsgebiedContent:
         Point to existing ambtsgebied if available in state.
         """
         # TODO: currently generates new OW ID every time. Read from input data if unchanged
-        ow_id: str = generate_ow_id(IMOWTYPES.REGELINGSGEBIED, self.provincie_id)
+        ow_id: str = generate_ow_id(IMOWTYPES.REGELINGSGEBIED, self._provincie_id)
         regelingsgebied = OWRegelingsgebied(
             OW_ID=ow_id,
-            ambtsgebied=self.ambtsgebied,
+            ambtsgebied=self._ambtsgebied,
             procedure_status=self.ow_procedure_status,
         )
         return regelingsgebied
@@ -61,7 +64,7 @@ class OwRegelingsgebiedContent:
         content = load_template(
             "ow/owRegelingsgebied.xml",
             pretty_print=True,
-            data=self.xml_data,
+            data=self._xml_data,
         )
         output_file = OutputFile(
             filename="owRegelingsgebied.xml",
