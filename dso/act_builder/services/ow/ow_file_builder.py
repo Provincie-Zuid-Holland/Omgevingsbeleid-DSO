6dd5db24e0e099ca0ad5d6229eb6a86f5ebd040c
awiseguy <arvin@wise-it.info>
2024-05-28 15:03:39 +0200

    duplicate schema removed

--- dso/act_builder/services/ow/ow_file_builder.py
+++ dso/act_builder/services/ow/ow_file_builder.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Any, Dict
 
-
 from ....models import ContentType
 from ....services.utils.helpers import load_template
 from ...state_manager.models import OutputFile, StrContentData
