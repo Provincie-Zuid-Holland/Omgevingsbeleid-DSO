3db3a86ee8e05516cde2dfe14463eac3731ab8a5
awiseguy <arvinwijsman@gmail.com>
2024-10-22 11:34:03 +0200

    added output file test cases

--- tests/functional/test_output_files.py
+++ tests/functional/test_output_files.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 
 from dso.services.ow.models import OWGebiedenGroep, OWGebied, OWDivisieTekst, OWDivisie
 
-from .base import BaseTestBuilder
+from .base import BaseTestBuilder, TEST_SCENARIO_DIRS
 
 
 @pytest.fixture(scope="class")
@@ -37,33 +37,49 @@ def namespaces():
         "ow": "http://www.geostandaarden.nl/imow/owobject",
         "rg": "http://www.geostandaarden.nl/imow/regelingsgebied",
         "ow-dc": "http://www.geostandaarden.nl/imow/bestanden/deelbestand",
-        "lvbb": "http://www.overheid.nl/2017/lvbb"
+        "lvbb": "http://www.overheid.nl/2017/lvbb",
+        "basisgeo": "http://www.geostandaarden.nl/basisgeometrie/1.0",
+        "gio": "https://standaarden.overheid.nl/stop/imop/gio/",
+        "geo": "https://standaarden.overheid.nl/stop/imop/geo/",
+        "data": "https://standaarden.overheid.nl/stop/imop/data/",
+        "gml": "http://www.opengis.net/gml/3.2",
+        "rg": "http://www.geostandaarden.nl/imow/regelingsgebied",
+        "tekst": "https://standaarden.overheid.nl/stop/imop/tekst/",
     }
     return namespaces
 
 
-@pytest.mark.parametrize(
-    "input_dir",
-    [
-        "tests/functional/scenarios/01-initial",
-    ],
-    indirect=True,
-)
+@pytest.mark.parametrize("input_dir", TEST_SCENARIO_DIRS, indirect=True)
 class TestPackageOutputFiles(BaseTestBuilder):
     def test_expected_files_exist(self, expected_results):
         """
-        Ensure all expected files from package_files list are found in the output directory. 
+        Ensure all expected files from package_files list are found in the output directory.
         """
         expected_files = expected_results["package_files"]
         assert self.output_dir is not None, "Output directory not set"
         for file in expected_files:
             assert (self.output_dir / file).exists(), f"Expected file {file} not found in output directory"
 
+    def test_expected_wids_used(self, expected_results, namespaces):
+        file_list = expected_results["package_files"]
+        # find single file that starts with pattern akn_ and ends with .xml
+        bill_file = next(file for file in file_list if file.startswith("akn_") and file.endswith(".xml"))
+
+        tree = etree.parse(f"{self.output_dir}/{bill_file}", parser=None)
+        root = tree.getroot()
+        wids = expected_results["wids_used"]
+        for wId in wids:
+            assert root.xpath(
+                f".//*[@wId='{wId}']", namespaces=namespaces
+            ), f"Expected wId {wId} not found in {bill_file}"
+
+        assert True
+
     def test_manifest_content(self, expected_results, namespaces):
         expected_manifest = expected_results["package_files"]
 
         manifest_results = []
-        tree = etree.parse(f"{self.output_dir}/manifest.xml")
+        tree = etree.parse(f"{self.output_dir}/manifest.xml", parser=None)
         root = tree.getroot()
 
         for bestand in root.findall(".//lvbb:bestand", namespaces=namespaces):
@@ -73,21 +89,15 @@ class TestPackageOutputFiles(BaseTestBuilder):
 
         # ensure every expected file is found in the manifest bestandsnaam
         for file in expected_manifest:
-            assert any(file in result["bestandsnaam"] for result in manifest_results), f"Expected file {file} not found in manifest"
+            assert any(
+                file in result["bestandsnaam"] for result in manifest_results
+            ), f"Expected file {file} not found in manifest"
 
-    def test_manifest_ow_content(self, expected_results, namespaces):
-        # TODO
-        assert True
-
-    def test_regelinggebied_content(self, expected_results, namespaces):
-        # TODO
-        assert True
-
-    def test_ow_gebieden(self, expected_results, namespaces):
+    def test_expected_ow_gebied_objects(self, expected_results, namespaces):
         expected_gebieden = expected_results["owLocaties"]["gebieden"]
 
         # Parse the XML file
-        tree = etree.parse(f"{self.output_dir}/owLocaties.xml")
+        tree = etree.parse(f"{self.output_dir}/owLocaties.xml", parser=None)
         root = tree.getroot()
 
         for expected_gebied in expected_gebieden:
@@ -120,13 +130,87 @@ class TestPackageOutputFiles(BaseTestBuilder):
             ), f"GebiedRef href does not match for Gebiedengroep '{noemer}'"
 
     def test_ow_ambtsgebied(self, expected_results, namespaces):
+        """ensure any ambtsgebied changes reflect in both owlocaties and owregelingsgebied"""
         expected_ambtsgebied = expected_results["owLocaties"]["ambtsgebied"]
 
         # Parse the XML file
         tree = etree.parse(f"{self.output_dir}/owLocaties.xml")
         root = tree.getroot()
 
+        ambtsgebied = root.xpath(".//l:Ambtsgebied", namespaces=namespaces)
+
         if expected_ambtsgebied:
-            ambtsgebied = root.xpath(".//l:Ambtsgebied", namespaces=namespaces)
             assert len(ambtsgebied) == 1, f"Expected one Ambtsgebied, found {len(ambtsgebied)}"
-            ambtsgebied = ambtsgebied[0]
+        else:
+            assert len(ambtsgebied) == 0, "Expected no Ambtsgebied, but found one"
+
+    def test_gml_ids_match_ow(self, expected_results, namespaces):
+        """
+        check if every expected geo identifier is used in .gml files +
+        ow locatie object.
+        """
+        expected_geo_identifiers = expected_results["geo"]["identifiers"]
+
+        geo_ids = []
+        gml_files = [file for file in expected_results["package_files"] if file.endswith(".gml")]
+        for gml_file in gml_files:
+            tree = etree.parse(f"{self.output_dir}/{gml_file}", parser=None)
+            gml_root = tree.getroot()
+            geometry = gml_root.xpath(".//basisgeo:Geometrie[basisgeo:id]", namespaces=namespaces)
+            geo_ids.append(geometry[0][0].text)  # extract ID
+
+        assert geo_ids == expected_geo_identifiers, "GML IDs do not match expected geo identifiers"
+
+        tree = etree.parse(f"{self.output_dir}/owLocaties.xml", parser=None)
+        ow_root = tree.getroot()
+        ow_gebieden = ow_root.xpath(".//l:Gebied", namespaces=namespaces)
+        ow_gio_refs = []
+        for ow in ow_gebieden:
+            ref = ow.find(".//l:GeometrieRef", namespaces=namespaces).get("{http://www.w3.org/1999/xlink}href")
+            ow_gio_refs.append(ref)
+
+        assert ow_gio_refs == expected_geo_identifiers, "GML IDs do not match OW GeometrieRefs"
+
+    def test_gio_uris(self, expected_results, namespaces):
+        expected_gio_uris = expected_results["geo"]["uris"]
+
+        gio_files = [
+            file for file in expected_results["package_files"] if file.startswith("GIO_") and file.endswith(".xml")
+        ]
+        output_gio_uris = []
+        for gio_file in gio_files:
+            tree = etree.parse(f"{self.output_dir}/{gio_file}", parser=None)
+            gio_root = tree.getroot()
+            output_gio_uris.append(gio_root.find(".//data:FRBRExpression", namespaces=namespaces).text)
+
+        # ensure for every expected gio uri, only 1 corresponding GIO file exists
+        assert output_gio_uris == expected_gio_uris, "GIO URIs do not match expected URIs"
+
+        bill_file = next(
+            file for file in expected_results["package_files"] if file.startswith("akn_") and file.endswith(".xml")
+        )
+
+        tree = etree.parse(f"{self.output_dir}/{bill_file}", parser=None)
+        root = tree.getroot()
+
+        # check io refs in bill file
+        informatieobject_refs = root.findall(
+            ".//data:informatieobjectRefs/data:informatieobjectRef", namespaces=namespaces
+        )
+        for ref in informatieobject_refs:
+            assert ref.text in expected_gio_uris, f"<informatieobjectRef> {ref.text} does not match expected GIO list"
+
+        # check consolidatie informatie in bill file
+        instrument_versies = root.findall(
+            ".//data:ConsolidatieInformatie//data:instrumentVersie", namespaces=namespaces
+        )
+        consolidatie_instr_versies = [iv.text for iv in instrument_versies]
+
+        for uri in expected_gio_uris:
+            assert uri in consolidatie_instr_versies, f"Expected URI {uri} not found in <instrumentVersie> elements"
+
+        # check tekst reference
+        io_refs = root.findall(".//tekst:ExtIoRef", namespaces=namespaces)
+        io_ref_attrs = [io_ref.get("ref") for io_ref in io_refs]
+        for uri in expected_gio_uris:
+            assert uri in io_ref_attrs, f"Expected URI {uri} not found in <ExtIoRef> attributes"
