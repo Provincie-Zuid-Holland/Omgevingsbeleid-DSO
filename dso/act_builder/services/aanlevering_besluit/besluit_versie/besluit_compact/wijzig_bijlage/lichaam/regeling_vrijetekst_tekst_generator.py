7bd3247e5bfe57515c588e65526f36481f92501d
awiseguy <arvin@wise-it.info>
2025-01-10 13:06:43 +0800

    restore conflict deleted lines

--- dso/act_builder/services/aanlevering_besluit/besluit_versie/besluit_compact/wijzig_bijlage/lichaam/regeling_vrijetekst_tekst_generator.py
+++ dso/act_builder/services/aanlevering_besluit/besluit_versie/besluit_compact/wijzig_bijlage/lichaam/regeling_vrijetekst_tekst_generator.py
@@ -37,8 +37,15 @@ class RegelingVrijetekstTekstGenerator:
         tekst = self._add_ewids(tekst)
         self._set_debug("text-stage-ewids", tekst)
 
+        object_code_lookup: Dict[str, ObjectCodeElementData] = self._build_object_code_lookup(tekst)
+        tekst = self._resolve_object_intrefs(tekst, object_code_lookup)
+        self._set_debug("text-stage-object-intrefs", tekst)
+
         tekst = self._resolve_intiorefs(tekst)
-        self._set_debug("text-stage-intiorefs", tekst)
+        self._set_debug("text-stage-annotation", tekst)
+
+        tekst = self._set_document_refs(tekst)
+        self._set_debug("text-stage-document-refs", tekst)
 
         tekst = self._remove_hints(tekst)
         self._set_debug("text-stage-deleted-hints", tekst)
@@ -91,6 +98,57 @@ class RegelingVrijetekstTekstGenerator:
         result: str = self._state_manager.act_ewid_service.add_ewids(xml_data)
         return result
 
+    def _set_document_refs(self, xml_data: str) -> str:
+        document_wid_lookup = self._state_manager.document_wid_lookup
+
+        parser = etree.XMLParser(remove_blank_text=False, encoding="utf-8")
+        root = etree.fromstring(xml_data.encode("utf-8"), parser)
+
+        for document_intio in root.xpath("//IntIoRef[@data-hint-document-uuid]"):
+            document_uuid = document_intio.get("data-hint-document-uuid")
+            if document_uuid in document_wid_lookup:
+                ref_value = document_wid_lookup[document_uuid]
+                document_intio.set("ref", ref_value)
+
+        output: str = etree.tostring(root, pretty_print=False, encoding="utf-8").decode("utf-8")
+        return output
+
+    def _build_object_code_lookup(self, xml_data: str) -> Dict[str, ObjectCodeElementData]:
+        result: Dict[str, ObjectCodeElementData] = {}
+
+        parser = etree.XMLParser(remove_blank_text=False, encoding="utf-8")
+        root = etree.fromstring(xml_data.encode("utf-8"), parser)
+        for object_element in root.xpath("//*[@data-hint-object-code]"):
+            object_code: str = object_element.get("data-hint-object-code")
+            eId: str = object_element.get("eId", "")
+            wId: str = object_element.get("wId", "")
+            tag_name: str = object_element.tag
+
+            result[object_code] = ObjectCodeElementData(
+                object_code=object_code,
+                wid=wId,
+                eid=eId,
+                tag_name=tag_name,
+            )
+
+        return result
+
+    def _resolve_object_intrefs(self, xml_data: str, object_code_lookup: Dict[str, ObjectCodeElementData]) -> str:
+        parser = etree.XMLParser(remove_blank_text=False, encoding="utf-8")
+        root = etree.fromstring(xml_data.encode("utf-8"), parser)
+
+        for document_intref in root.xpath("//IntRef[@data-hint-target-object-code]"):
+            object_code: str = document_intref.get("data-hint-target-object-code")
+            if object_code not in object_code_lookup:
+                raise RuntimeError(f"Trying to create internal link to unknown Object Code `{object_code}`")
+
+            data = object_code_lookup[object_code]
+            document_intref.set("ref", data.eid)
+            document_intref.set("scope", data.tag_name)
+
+        output: str = etree.tostring(root, pretty_print=False, encoding="utf-8").decode("utf-8")
+        return output
+
     def _resolve_intiorefs(self, xml_data: str) -> str:
         # updates IntIoRef.ref for gebiedsaanwijzingen after EWIDs are set.
         parser = etree.XMLParser(remove_blank_text=False, encoding="utf-8")
@@ -114,6 +172,7 @@ class RegelingVrijetekstTekstGenerator:
             "data-hint-target-object-code",
             "data-hint-wid-code",
             "data-hint-locatie",
+            "data-hint-document-uuid",
         ]
 
         root = etree.fromstring(xml_data)
