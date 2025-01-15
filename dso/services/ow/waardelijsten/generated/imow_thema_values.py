ab95f1e8a69e1370a0b7775ae5c40d5c5977180e
awiseguy <arvin@wise-it.info>
2025-01-15 11:38:19 +0800

    generated imow value lists

---
+++ dso/services/ow/waardelijsten/generated/imow_thema_values.py
@@ -0,0 +1,97 @@
+from ..imow_models import ThemaValue
+
+# Auto-generated from waardelijsten_IMOW JSON file
+# run cmds.py generate-imow-value-list command to create updated files.
+
+IMOW_THEMA_ITEMS = [
+    ThemaValue(
+        label="bodem",
+        term="Bodem",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/Bodem",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gericht zijn op bodem als aspect van de fysieke leefomgeving.",
+    ),
+    ThemaValue(
+        label="bouwwerken",
+        term="Bouwwerken",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/Bouwwerken",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gaan over bouwwerken.",
+    ),
+    ThemaValue(
+        label="cultureel erfgoed",
+        term="CultureelErfgoed",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/CultureelErfgoed",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gericht zijn op bescherming van cultureel erfgoed.",
+    ),
+    ThemaValue(
+        label="energie en natuurlijke hulpbronnen",
+        term="EnergieEnNatuurlijkeHulpbronnen",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/EnergieEnNatuurlijkeHulpbronnen",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gericht zijn op de benutting en bescherming van de energievoorziening en de natuurlijke hulpbronnen.",
+    ),
+    ThemaValue(
+        label="externe veiligheid",
+        term="ExterneVeiligheid",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/ExterneVeiligheid",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gericht zijn op het waarborgen van  de externe veiligheid.",
+    ),
+    ThemaValue(
+        label="geluid",
+        term="Geluid",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/Geluid",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gericht zijn op het tegengaan van geluidhinder.",
+    ),
+    ThemaValue(
+        label="gezondheid",
+        term="Gezondheid",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/Gezondheid",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gericht zijn op het waarborgen van de gezondheidsaspecten van de leefomgeving.",
+    ),
+    ThemaValue(
+        label="infrastructuur",
+        term="Infrastructuur",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/Infrastructuur",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gericht zijn op de infrastructuur als aspect van de fysieke leefomgeving.",
+    ),
+    ThemaValue(
+        label="landgebruik",
+        term="Landgebruik",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/Landgebruik",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gericht zijn op het aspect landgebruik.",
+    ),
+    ThemaValue(
+        label="landschap",
+        term="Landschap",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/Landschap",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gericht zijn op de bescherming en ontwikkeling van het landschap.",
+    ),
+    ThemaValue(
+        label="lucht",
+        term="Lucht",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/Lucht",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gericht zijn op lucht als aspect van de fysieke leefomgeving.",
+    ),
+    ThemaValue(
+        label="milieu algemeen",
+        term="MilieuAlgemeen",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/MilieuAlgemeen",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gericht zijn op risico's voor het milieu in algemene zin.",
+    ),
+    ThemaValue(
+        label="natuur",
+        term="Natuur",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/Natuur",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gericht zijn op natuur als aspect van de fysieke leefomgeving.",
+    ),
+    ThemaValue(
+        label="procedures",
+        term="Procedures",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/Procedures",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gericht zijn op procedures.",
+    ),
+    ThemaValue(
+        label="water en watersystemen",
+        term="WaterEnWatersystemen",
+        uri="http://standaarden.omgevingswet.overheid.nl/thema/id/concept/WaterEnWatersystemen",
+        definitie="Kenmerk dat aangeeft dat de regels of het beleid gericht zijn op water en watersystemen als aspect van de fysieke leefomgeving.",
+    ),
+]
---
+++ dso/services/ow/waardelijsten/generated/imow_type_gebiedsaanwijzing_values.py
@@ -0,0 +1,121 @@
+from ..imow_models import TypeGebiedsaanwijzingValue
+
+# Auto-generated from waardelijsten_IMOW JSON file
+# run cmds.py generate-imow-value-list command to create updated files.
+
+IMOW_TYPE_GEBIEDSAANWIJZING_ITEMS = [
+    TypeGebiedsaanwijzingValue(
+        label="beperkingengebied",
+        term="Beperkingengebied",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Beperkingengebied",
+        definitie="Object dat machineleesbaar maakt dat een Juridische regel en de bijbehorende Locatie(s) gaan over een beperkingengebied als bedoeld in de Omgevingswet: een bij of krachtens de wet aangewezen gebied waar, vanwege de aanwezigheid van een werk of object, regels gelden over activiteiten die gevolgen hebben of kunnen hebben voor dat werk of object.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="bodem",
+        term="Bodem",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Bodem",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op de bescherming van de bodemkwaliteit.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="bouw",
+        term="Bouw",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Bouw",
+        definitie="Object dat machineleesbaar maakt dat een Juridische regel in het omgevingsplan en de bijbehorende Locatie(s) gaan over de situering van bouwwerken.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="defensie",
+        term="Defensie",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Defensie",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op de bescherming en het tegengaan van verstoring van militaire gebieden en objecten.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="energievoorziening",
+        term="Energievoorziening",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Energievoorziening",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op de bescherming en bevordering van de energievoorziening.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="erfgoed",
+        term="Erfgoed",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Erfgoed",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op de bescherming van cultureel erfgoed.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="externe veiligheid",
+        term="ExterneVeiligheid",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/ExterneVeiligheid",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op het waarborgen van de veiligheid.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="functie",
+        term="Functie",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Functie",
+        definitie="Object dat machineleesbaar maakt dat een Juridische regel en de bijbehorende Locatie(s) gaan over een functie: het gebruiksdoel of de bijzondere eigenschap die een onderdeel van de fysieke leefomgeving op een bepaalde locatie heeft.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="geluid",
+        term="Geluid",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Geluid",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op het tegengaan van geluidhinder.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="geur",
+        term="Geur",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Geur",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op het tegengaan van geurhinder.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="landschap",
+        term="Landschap",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Landschap",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op de bescherming en de ontwikkeling van het landschap vanuit ander perspectief dan natuur en erfgoed.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="leiding",
+        term="Leiding",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Leiding",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op het waarborgen van de goede staat en instandhouding van leidingen.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="lucht",
+        term="Lucht",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Lucht",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op de bescherming van de kwaliteit van de buitenlucht.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="mijnbouw",
+        term="Mijnbouw",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Mijnbouw",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op het kunnen verrichten van mijnbouwactiviteiten.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="natuur",
+        term="Natuur",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Natuur",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op de bescherming van natuur en landschap.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="recreatie",
+        term="Recreatie",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Recreatie",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op de beheersing en ontwikkeling van recreatie.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="ruimtelijk gebruik",
+        term="RuimtelijkGebruik",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/RuimtelijkGebruik",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op die vormen van ruimtelijk gebruik die niet onder een van de andere Gebiedsaanwijzingtypen te vatten zijn.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="verkeer",
+        term="Verkeer",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/Verkeer",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op beheer, onderhoud en ontwikkeling van verkeer en mobiliteit.",
+    ),
+    TypeGebiedsaanwijzingValue(
+        label="water en watersysteem",
+        term="WaterEnWatersysteem",
+        uri="http://standaarden.omgevingswet.overheid.nl/typegebiedsaanwijzing/id/concept/WaterEnWatersysteem",
+        definitie="Object dat machineleesbaar maakt dat een gebied is aangewezen door regels of beleid, gericht op het beheer van water en watersystemen.",
+    ),
+]
