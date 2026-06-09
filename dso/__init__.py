e7a16b8962d55c8298dc998cb0c61e4c3121c2d1
Max Remkes <21196963+maxrem@users.noreply.github.com>
2026-06-09 15:59:21 +0200

    expose ThemaFactory, Thema

--- dso/__init__.py
+++ dso/__init__.py
@@ -1,2 +1,4 @@
 import dso.services.ow.gebiedsaanwijzingen.types as ad
-from dso.services.ow.gebiedsaanwijzingen.gebiedsaanwijzing import GebiedsaanwijzingenFactory, Gebiedsaanwijzingen
+from dso.services.ow.gebiedsaanwijzingen.gebiedsaanwijzing import Gebiedsaanwijzingen, GebiedsaanwijzingenFactory
+from dso.services.ow.themas.thema import ThemaFactory
+from dso.services.ow.themas.types import Thema
